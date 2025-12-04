e4633b771376e9898181d59397c4fcba751207ae
Faiz Akram <172576315+FaizAkram19@users.noreply.github.com>
2025-12-04 06:12:45 +0000

    logic to play the game in the console is done

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ const GameController = (function() {
     { name: "Player One", marker: "X" },
     { name: "Player Two", marker: "O" }
   ];
+  const winCondition=[[0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6]];
 
   let activePlayer = players[0] ; 
 
@@ -34,12 +42,40 @@ const GameController = (function() {
     Gameboard.printBoard();
     console.log(`${getActivePlayer().name}'s turn.`);
   };
+  const resetBoard=()=>{
+    const board=Gameboard.getBoard();
+    for(i=0;i<9;i++)
+    {
+      board[i]="";
+    }
+  }
+  const checkWinner=()=>{
+    let activePlayer=getActivePlayer();
+    const board=Gameboard.getBoard();
+    for(i=0;i<winCondition.length;i++)
+    {
+      const [a,b,c]=winCondition[i];
+      if(board[a]==activePlayer.marker &&
+        board[b]==activePlayer.marker &&
+        board[c]==activePlayer.marker)
+        {
+          console.log(`${activePlayer.name} won.`);
+          resetBoard();
+        }
+    }
+  }
 
   const playRound = (index) => {
+    const board=Gameboard.getBoard();
+    if(board[index] != "")
+    {
+      console.log(`Index ${index} is not empty, shoose another spot`);
+      return ;
+    }
     console.log(`Dropping ${getActivePlayer().name}'s token into spot ${index}...`);
     
     Gameboard.placeMarker(index,activePlayer.marker);
-    // Step B: Check for a winner (OPTIONAL: You can skip this for now to just get the game loop working)
+    checkWinner();
     
     switchPlayerTurn();
     printNewRound();
